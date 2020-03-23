4d9e8ed6488a8141c710c02b5baa946bb1f09b68
AshDev <mrashtonka@gmail.com>
2020-03-24 00:05:35 +1100

    Java Doc comments and MIT License added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,31 @@
-import Config.BotToken;
-import Events.GuildEvent;
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 AshDev (Ashley Tonkin)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 import com.jagrosh.jdautilities.command.CommandClientBuilder;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
+import config.BotToken;
+import events.GuildEvent;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
@@ -12,10 +36,20 @@ import org.slf4j.LoggerFactory;
 
 import javax.security.auth.login.LoginException;
 
+/**
+ * The type Main.
+ */
 public class Main {
-    private static BotToken botToken = new BotToken();
+    private static final BotToken botToken = new BotToken();
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     * @throws LoginException       the login exception
+     * @throws InterruptedException the interrupted exception
+     */
     public static void main(String[] args) throws LoginException, InterruptedException {
 
         EventWaiter waiter = new EventWaiter();
---
+++ src/main/java/events/GuildEvent.java
@@ -0,0 +1,57 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 AshDev (Ashley Tonkin)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package events;
+
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The type Guild event.
+ */
+public class GuildEvent extends ListenerAdapter {
+
+    private static final Logger log = LoggerFactory.getLogger(GuildEvent.class);
+
+    /**
+     * On guild member join.
+     *
+     * @param event the event
+     */
+    @Override
+    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+        log.info("{} has joined: {}", event.getUser().getName(), event.getGuild().getName());
+        User user = event.getUser();
+        user.openPrivateChannel().queue(
+                privateChannel -> {
+                    privateChannel.sendMessageFormat("Message goes here!").queue();
+                    log.info("Private Message has been sent");
+                }
+        );
+    }
+
+}
